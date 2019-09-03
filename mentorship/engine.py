5e62c0c1c674de7a1ee7520d4f8b1f426f4fcd12
Jean-Mark Wright <jeanmark.wright@gmail.com>
2019-09-02 23:23:11 -0400

    Allowed debugging in the pipeline

--- mentorship/engine.py
+++ mentorship/engine.py
@@ -1,14 +1,24 @@
+import os
 from typing import Callable, List
 
 from pandas import DataFrame
+from termcolor import cprint
 
 Step = Callable[[DataFrame], DataFrame]
+
+DEBUG_PIPELINE = bool(int(os.getenv("DEBUG_PIPELINE", "0")))
 
 
 def run_pipeline(input_data: DataFrame, steps: List[Step]) -> DataFrame:
     output = input_data
-    for step in steps:
+    for index, step in enumerate(steps):
+        cprint(f">>>> Running step {index + 1}: {step.__name__}\n", "green")
         output = step(output)
+        if DEBUG_PIPELINE:
+            import pdb
+
+            pdb.set_trace()
+        print("")
     return output
 
 
